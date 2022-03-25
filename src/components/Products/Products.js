72f015987e660387416d84683cdd18f60327c0a1
ZeroBug <404.zerobug@gmail.com>
2022-03-25 23:31:17 +0600

    Add to cart button added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import './Products.css'
+import React from "react";
+import "./Products.css";
 
 const Products = (props) => {
-    const {id, name, picture, price} = props.product
-    return (
-        <div className='product'>
-            <img src={picture} alt="" />
-            <div className='product-info'>
-                <h3>Name: {name}</h3>
-                <h4>Price: ${price}</h4>
-            </div>
-            <button className='btn-cart'>
-                <p>Add To Cart</p>
-            </button>
-        </div>
-    );
+  const { id, name, picture, price } = props.product;
+  return (
+    <div className="product">
+      <img src={picture} alt="" />
+      <div className="product-info">
+        <h3>Name: {name}</h3>
+        <h4>Price: ${price}</h4>
+      </div>
+      <button className="btn-cart">
+        <p>Add To Cart</p>
+      </button>
+    </div>
+  );
 };
 
 export default Products;
